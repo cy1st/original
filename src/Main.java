95f6bebba735063122b009e23aee7ae5a1163daf
cxy <619995497@qq.com>
2023-12-26 16:14:26 +0800

    init

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * 该项目为基于jdk1.8为基础，进行基础编码练习
+ *
+ * @author cxy
+ * @since 2023/12/26 15:19
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
